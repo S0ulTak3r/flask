e4bd4297f8bb924a2cf790b16db9e2dc5797cd73
S0ulTak3r <danielbusinessboguslavsky@gmail.com>
2023-07-09 23:33:40 +0300

    l

--- mysql/sql-scripts/1_init.sql
+++ mysql/sql-scripts/1_init.sql
@@ -7,15 +7,8 @@ CREATE TABLE images(
 
 -- Insert the cat images with URLs and descriptions
 INSERT INTO images(url, description) VALUES
-('https://media.giphy.com/media/yr7n0u3qzO9nG/giphy.gif', 'a'),
-('https://media.tenor.com/rMLswxLq2uEAAAAM/funny-as.gif', 'a'),
-('https://i.gifer.com/QPt.gif', 'a'),
-('https://media1.giphy.com/media/mC7VjtF9sYofs9DUa5/200w.gif?cid=6c09b9521pven9h01wypu84n9jt6eg2q87g01vl3mlkikduj&ep=v1_gifs_search&rid=200w.gif&ct=g', 'a'),
-('https://media0.giphy.com/media/kbuQOkATEo6VW/giphy.gif', 'a'),
-('https://media.tenor.com/lSqAQeWHBQAAAAAC/sid-ice.gif', 'a'),
-('https://thumbs.gfycat.com/CalculatingEcstaticDwarfrabbit-size_restricted.gif', 'a'),
-('https://media1.giphy.com/media/H5C8CevNMbpBqNqFjl/giphy.gif', 'a'),
-('https://media.tenor.com/-o7IJJHaE5MAAAAM/kto-kounotoritoken.gif', 'a'),
-('https://media.giphy.com/media/yr7n0u3qzO9nG/giphy.gif', 'a'),
-('https://media.tenor.com/rMLswxLq2uEAAAAM/funny-as.gif', 'a'),
-('https://media.tenor.com/-o7IJJHaE5MAAAAM/kto-kounotoritoken.gif', 'a');
+('https://media.tenor.com/Wi9uNKlPjZ0AAAAM/american-psycho-smoke.gif', 'a'),
+('https://media.tenor.com/TDSHLyUWghwAAAAd/american-psycho-tense.gif', 'a'),
+('https://i.pinimg.com/originals/c5/52/8e/c5528e6c4bb0a0ed0b7a3fcf127c68a2.gif', 'a'),
+('https://media.tenor.com/GxA2qm-qmlkAAAAM/meme-american-psycho.gif', 'a'),
+('https://media.tenor.com/xV70qxxgM14AAAAM/patrick-bateman.gif', 'a');

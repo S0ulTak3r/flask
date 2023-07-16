3e753f5ba81f3b485e51126605bd142a480cbd5d
S0ulTak3r <danielbusinessboguslavsky@gmail.com>
2023-07-16 14:12:30 +0300

    l

---
+++ flask-app/CSS/second_intro.js
@@ -0,0 +1,29 @@
+const nameElem = document.querySelector('.name');
+const questionElem = document.querySelector('.question');
+const optionsElem = document.querySelector('.options');
+const bioYesElem = document.querySelector('.bio-yes');
+const bioNoElem = document.querySelector('.bio-no');
+const introContainerElem = document.querySelector('.intro-container');
+
+nameElem.style.display = 'block';
+nameElem.addEventListener('animationend', () => {
+    questionElem.style.display = 'block';
+});
+
+function showBio(isYes) {
+    optionsElem.style.display = 'none';
+    if (isYes) {
+        bioYesElem.style.display = 'block';
+        bioNoElem.style.display = 'none';
+    } else {
+        bioYesElem.style.display = 'none';
+        bioNoElem.style.display = 'block';
+    }
+    setTimeout(() => {
+        introContainerElem.style.display = 'block';
+    }, 10000);
+}
+
+setTimeout(() => {
+    window.location.href = "/welcome";
+}, 30000);
